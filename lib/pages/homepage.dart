789cb82e97265980586288bab4c89ae3e068414a
1bnunugroho <ibnu.nugroho@heli.id>
2019-06-09 01:08:36 +0700

    initial background

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        decoration: _renderBackground(),
+      ),
+    );
+  }
+
+  BoxDecoration _renderBackground() {
+    return new BoxDecoration(
+      gradient: new LinearGradient(
+          colors: [Colors.grey[200], Colors.grey[300]],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp),
+    );
+  }
+}
